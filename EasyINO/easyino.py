a952e9910a55eea8736d0f44a90c5dd2d4604b5b
tkokko95 <taavi.kokko@gmail.com>
2021-02-19 12:22:37 +0200

    Linting

--- EasyINO/easyino.py
+++ EasyINO/easyino.py
@@ -1,29 +1,29 @@
-from colorama import init, Fore, Style
 import subprocess
-from sys import argv
 import argparse
+from colorama import init, Fore, Style
 
-def get_output(cmd : str):
+
+def get_output(command : str):
     """Get output of a command as utf8"""
 
-    return subprocess.run(cmd.split( ), capture_output=True, encoding='utf8').stdout
+    return subprocess.run(command.split( ), capture_output=True, encoding='utf8',check=True).stdout
 
-def parse_boards():
+def parse_boards(output, board_filter, upload_mode):
     """A terrible mess of a function to parse the boards and their IDs into a list of dicts"""
     stripped_lines = []
-    boardlist = []
+    boards = []
 
     # Split the output by lines
-    lines = cmdout.splitlines()
+    lines = output.splitlines()
 
     # Strip whitespaces
     # The first and the last entries in the list are useless so cut them
     for line in lines[1:-1]:
-            stripped_lines.append(line.strip())
+        stripped_lines.append(line.strip())
 
     # Get the board FQBN
     for line in stripped_lines:
-        if not args.board or line.lower().find(args.board) != -1:
+        if not board_filter or line.lower().find(board_filter) != -1:
             line = line.split(' ')
 
             # If we list all, we only need the last column and we don't care
@@ -31,20 +31,20 @@ def parse_boards():
 
             # If we list connected, we need the first (port) and second
             # from the right (FBQN)
-            if not args.upload:
-                boardlist.append((line[-1], None))
+            if not upload_mode:
+                boards.append((line[-1], None))
             else:
                 if line[-2]:
-                    boardlist.append((line[-2], line[0]))
+                    boards.append((line[-2], line[0]))
                 else:
-                    boardlist.append(('[UNKNOWN]', line[0]))
+                    boards.append(('[UNKNOWN]', line[0]))
 
-    #Return a tuple, in format (FBQN, Port)
-    return(boardlist)
+    #Return a list of tuples, in format (FBQN, Port)
+    return boards
 
-def choose_board():
+def choose_board(boards):
     """List available boards, ask for choice and return the selected board"""
-    for board in boardlist:
+    for board in boards:
         print(Style.BRIGHT + f'\n{boardlist.index(board)}: {board[0]}' + Style.RESET_ALL)
         if args.upload:
             print(Style.BRIGHT + f'Port: {board[1]}' + Style.RESET_ALL)
@@ -59,12 +59,6 @@ def choose_board():
                 return boardlist[boardnum]
             else:
                 print(Fore.YELLOW + 'Invalid input' + Fore.WHITE)
-
-def compile(file, board, verbose):
-    pass
-
-def upload(file, board, verbose):
-    pass
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -83,20 +77,15 @@ if __name__ == '__main__':
         else:
             cmd = 'arduino-cli board listall'
 
-        cmdout = get_output()
-        boardlist = parse_boards()
+        cmdout = get_output(cmd)
+        boardlist = parse_boards(cmdout, args.board, args.upload)
         if boardlist:
-            board = choose_board()
+            sel_board = choose_board(boardlist)
             if args.upload:
                 pass
             else:
                 pass
-
         else:
             print(Fore.RED + 'No boards found' + Style.RESET_ALL)
     except KeyboardInterrupt:
         print(Fore.RED + '\nAborted (CTRL + C)' + Style.RESET_ALL)
-
-
-
-
